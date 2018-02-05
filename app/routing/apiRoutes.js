9747185621b632525cd89dfb779f695e1e2c07bf
Keissy <keissy@lifecellskincare.com>
2018-02-05 16:23:16 -0500

    first commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@ var friends = require('../data/friends.js');
 
 // Export API routes
 module.exports = function(app) {
-	// console.log('___ENTER apiRoutes.js___');
+	console.log('___ENTER apiRoutes.js___');
 
 	// Total list of friend entries
 	app.get('/api/friends', function(req, res) {
